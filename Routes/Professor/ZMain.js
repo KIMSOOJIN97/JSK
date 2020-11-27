96e933bc2fccff5d1f3aa4854728361ba3aa804c
LeeKangPoong <dlrkdvnd@gmail.com>
2020-11-28 04:35:56 +0900

    pooong3

--- Routes/Professor/ZMain.js
+++ Routes/Professor/ZMain.js
@@ -4,11 +4,7 @@ var router = express.Router();
 
 
 router.get('/', function(req, res){
-<<<<<<< HEAD
     console.log('ZMain 접속');
-=======
-    console.log('Prof_Main 접속');
->>>>>>> 83a79bc9fc856e1258662c303e777278cfb92f3b
 
     if(req.session.user) {
         var context = {userid:req.session.user.id};
@@ -18,10 +14,7 @@ router.get('/', function(req, res){
         var context = {userid:'No_One'};
         res.render('ZMain', context);
     }
-<<<<<<< HEAD
     
-=======
->>>>>>> 83a79bc9fc856e1258662c303e777278cfb92f3b
 });
 
 module.exports = router;
